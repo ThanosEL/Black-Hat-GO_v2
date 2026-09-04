643944ce7bfc9ba2e692a04e74939caec55d5568
ThanosEL <thanos.fedora.99@gmail.com>
2026-09-04 11:33:57 +0300

    finishing the tcp-scanner final version

---
+++ 2_TCP_Scanners&Proxies/scanners/workerPool-scanner.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func worker(ports chan int, wg *sync.WaitGroup) {
+	for p := range ports {
+		fmt.Println(p)
+		wg.Done()
+	}
+}
+
+func main() {
+	// second parameter, an int value of 100, this allows the channel to be buffered
+	ports := make(chan int, 100)
+	var wg sync.WaitGroup
+	// you use a for loop to start the desired number of workers
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, &wg)
+	}
+	for i := 1; i <= 1024; i++ {
+		wg.Add(1)
+		ports <- i
+	}
+	wg.Wait()
+	close(ports)
+}
